edfb9d528bc2ff4440a8417f9151993cc6035215
Lauri Laaksonen <laaksonel@gmail.com>
2023-03-18 13:31:50 +0200

    Generate account context

---
+++ rumbl/test/support/fixtures/accounts_fixtures.ex
@@ -0,0 +1,20 @@
+defmodule Rumbl.AccountsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Rumbl.Accounts` context.
+  """
+
+  @doc """
+  Generate a user.
+  """
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        name: "some name"
+      })
+      |> Rumbl.Accounts.create_user()
+
+    user
+  end
+end
